080259f56b85f71a1c05df4051c50e8f5f1cab77
AlexLiCa <alexlin.carre@gmail.com>
2022-11-08 17:18:47 -0600

    Titulo

--- api/src/App.js
+++ api/src/App.js
@@ -1,14 +1,38 @@
 import './App.css';
 import Personajes from './components/Personajes';
+import Tarjeta from './components/Tarjeta'
 
 
 function App() {
+  let rickImg = "https://www.looper.com/img/gallery/the-other-back-to-the-future-reference-you-missed-in-rick-and-morty/l-intro-1618319353.jpg"
   return (
     <div className="App">
-      <header className="bg">
-        <h1 className="card">Rick and Morty</h1>
-        <Personajes/>
-      </header>
+      <div className="bg">
+      <div className="container">
+        <h1 className="card-s">Personajes Rick y Morty</h1>
+      </div>
+        <div className="container">
+          <div className="row">
+            <Tarjeta img={rickImg} txt="Rick"/>
+          </div>
+          <div className="row">
+            <Tarjeta img={rickImg} txt="Rick"/>
+          </div>
+          <div className="row">
+            <Tarjeta img={rickImg} txt="Rick"/>
+          </div>
+          <div className="row">
+            <Tarjeta img={rickImg} txt="Rick"/>
+          </div>
+          <div className="row">
+            <Tarjeta img={rickImg} txt="Rick"/>
+          </div>
+          <div className="row">
+            <Tarjeta img={rickImg} txt="Rick"/>
+          </div>
+        </div>
+        {/* <Personajes/> */}
+      </div>
     </div>
   );
 }
---
+++ api/src/components/Tarjeta.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const Tarjeta = ({img, txt}) => {
+    return ( 
+    <div className="card">
+        <img src={img} width="250px" height="200px"/>
+        <h1>{txt}</h1>
+      </div>
+
+     );
+}
+ 
+export default Tarjeta;
