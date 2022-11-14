68eb062b683d133cb2c8d1d283b5acec331c0f31
AlexLiCa <alexlin.carre@gmail.com>
2022-11-14 13:56:33 -0600

    version final

--- api/src/components/Nav.js
+++ api/src/components/Nav.js
@@ -6,12 +6,13 @@ const Nav = ({pag, setPag}) => {
         <div className="container-l">
             <nav>
                 <div class="topnav">
+                    <a className="active">{pag}</a>
                     <a onClick={()=> setPag(1)}>{"<<"}</a>
-                    <a onClick={()=> setPag(pag-1)}>Previous</a>
+                    <a onClick={()=> setPag(pag-1)}>{"<"}</a>
                     <a onClick={()=> setPag(pag+1)}>{pag+1}</a>
                     <a onClick={()=> setPag(pag+2)}>{pag+2}</a>
                     <a onClick={()=> setPag(pag+3)}>{pag+3}</a>
-                    <a onClick={()=> setPag(pag+1)}>Next</a>
+                    <a onClick={()=> setPag(pag+1)}>{">"}</a>
                     <a onClick={()=> setPag(42)}>{">>"}</a>
                 </div>
             </nav>
