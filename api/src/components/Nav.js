2238a5cc68e7427b5a5bdeb98cde378ccf5486fa
AlexLiCa <alexlin.carre@gmail.com>
2022-11-14 10:36:09 -0600

    correccion a links de la nav

--- api/src/components/Nav.js
+++ api/src/components/Nav.js
@@ -5,11 +5,11 @@ const Nav = () => {
     return ( 
         <nav>
             <div class="topnav">
-                <a class="active" href="#home">Previous</a>
-                <a href="#news">1</a>
-                <a href="#contact">2</a>
-                <a href="#about">3</a>
-                <a href="#about">Next</a>
+                <a class="active" href="#previous">Previous</a>
+                <a href="#1">1</a>
+                <a href="#2">2</a>
+                <a href="#3">3</a>
+                <a href="#next">Next</a>
             </div>
         </nav>
      );
