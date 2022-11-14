b860088cdfd82cec204fef04bc45ac25e5cac9a0
AlexLiCa <alexlin.carre@gmail.com>
2022-11-14 10:34:15 -0600

    Navbar v1

--- api/src/App.js
+++ api/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Personajes from './components/Personajes';
 import Tarjeta from './components/Tarjeta'
-
+import Nav from './components/Nav'
 
 function App() {
   let rickImg = "https://www.looper.com/img/gallery/the-other-back-to-the-future-reference-you-missed-in-rick-and-morty/l-intro-1618319353.jpg"
   return (
     <div className="App">
       <div className="bg">
+      <Nav/>
       <div className="container">
         <h1 className="card-s">Personajes de Rick y Morty</h1>
       </div>
--- api/src/components/Nav.js
+++ api/src/components/Nav.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import '../App.css';
 
 const Nav = () => {
     return ( 
-        <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-center">
-                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#">Next</a></li>
-            </ul>
+        <nav>
+            <div class="topnav">
+                <a class="active" href="#home">Previous</a>
+                <a href="#news">1</a>
+                <a href="#contact">2</a>
+                <a href="#about">3</a>
+                <a href="#about">Next</a>
+            </div>
         </nav>
-
      );
 }
  
