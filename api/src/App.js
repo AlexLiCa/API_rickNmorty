fb26b06868ab742f8511b5549106acde23c17cd4
AlexLiCa <alexlin.carre@gmail.com>
2022-11-14 13:31:01 -0600

    coleres de tarjetas

--- api/src/App.js
+++ api/src/App.js
@@ -11,12 +11,12 @@ function App() {
     return (
     <div className="App">
       <div className="bg">
-      <Nav pag={pag} setPag={setPag}/>
-      <div className="container">
-        <h1 className="card-s">Personajes de Rick y Morty</h1>
-      </div>
+        <Nav pag={pag} setPag={setPag}/>
+        <div className="container">
+          <h1 className="card-s">Personajes de Rick y Morty</h1>
+        </div>
         <Personajes pag={pag}/>
-      </div>
+        </div>
     </div>
   );
 }
--- api/src/components/Nav.js
+++ api/src/components/Nav.js
@@ -6,15 +6,16 @@ const Nav = ({pag, setPag}) => {
         <div className="container-l">
             <nav>
                 <div class="topnav">
+                    <a onClick={()=> setPag(1)}>{"<<"}</a>
                     <a onClick={()=> setPag(pag-1)}>Previous</a>
                     <a onClick={()=> setPag(pag+1)}>{pag+1}</a>
                     <a onClick={()=> setPag(pag+2)}>{pag+2}</a>
                     <a onClick={()=> setPag(pag+3)}>{pag+3}</a>
                     <a onClick={()=> setPag(pag+1)}>Next</a>
+                    <a onClick={()=> setPag(42)}>{">>"}</a>
                 </div>
             </nav>
         </div>
      );
 }
- 
 export default Nav;
