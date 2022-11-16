0ebba6195dbccbc7ed4f2d0d750e4b9b06990c1f
AlexLiCa <alexlin.carre@gmail.com>
2022-11-16 11:28:26 -0600

    validaciones de la navbar

--- api/src/components/Nav.js
+++ api/src/components/Nav.js
@@ -17,12 +17,24 @@ const Nav = ({pag, setPag, psj, setPsj}) => {
                 <div class="topnav">
                     <a className="active">{pag}</a>
                     <a onClick={()=> setPag(1)}>{"<<"}</a>
-                    <a onClick={()=> setPag(pag-1)}>{"<"}</a>
-                    <a onClick={()=> setPag(pag+1)}>{pag+1}</a>
-                    <a onClick={()=> setPag(pag+2)}>{pag+2}</a>
-                    <a onClick={()=> setPag(pag+3)}>{pag+3}</a>
-                    <a onClick={()=> setPag(pag+1)}>{">"}</a>
-                    <a onClick={()=> setPag(42)}>{">>"}</a>
+                    <a onClick={()=> {pag === 1 ? (setPag(1)) : setPag(pag - 1)}}>
+                        {"<"}
+                    </a>
+                    <a onClick={()=> {pag === 42 ? (setPag(42)) : setPag(pag + 1)}}>
+                        {pag === 42 ? ("-") : (pag + 1)}
+                    </a>
+                    <a onClick={()=> {pag === 42 ? (setPag(42)) : setPag(pag + 2)}}>
+                    {pag === 42 ? ("-") : (pag + 2)}
+                    </a>
+                    <a onClick={()=> {pag === 42 ? (setPag(42)) : setPag(pag + 3)}}>
+                    {pag === 42 ? ("-") : (pag + 3)}
+                    </a>
+                    <a onClick={()=> {pag === 42 ? (setPag(42)) : setPag(pag + 1)}}>
+                        {pag === 42 ? ("-") : (">")}
+                    </a>
+                    <a onClick={()=> setPag(42)}>
+                        {pag === 42 ? ("-") : (">>")}
+                    </a>
                     <input type="text" id="psj" name="psj" value={psj} onChange={onChange}/>
                 </div>
             </nav>
